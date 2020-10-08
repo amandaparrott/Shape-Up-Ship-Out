2496bbc4cd51a85cc685d0cd0ef33642aae8db12
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-10-08 14:56:23 -0500

    js basics built

--- app.js
+++ app.js
@@ -1,6 +1,67 @@
 $(document).ready(function () {
 
+    rectangleButton = document.getElementById('rectangleButton'),
+        rectangleInputH = document.getElementById('rectangleInputH'),
+        rectangleInputW = document.getElementById('rectangleInputW'),
+        squareButton = document.getElementById('squarebutton'),
+        squareInput = document.getElementById('squareInput'),
+        circleButton = document.getElementById('circleButton'),
+        circleInput = document.getElementById('circleInput'),
+        triangleButton = document.getElementById('triangleButton'),
+        triangleInput = document.getElementById('triangleInput'),
+        shapeContainer = document.getElementById('shapeContainer'),
+        shapeInfo = document.getElementById('shapeInfo'),
+        widthInfo = document.getElementById('widthInfo'),
+        heightInfo = document.getElementById('heightInfo'),
+        radiusInfo = document.getElementById('radiusInfo'),
+        areaInfo = document.getElementById('areaInfo'),
+        perimeterInfo = document.getElementById('perimeterInfo');
 
+
+    class Shape {
+        constructor() {
+
+        }
+        describe() {
+
+        }
+    }
+
+    class Circle extends Shape {
+        constructor() {
+
+        }
+        describe() {
+
+        }
+    }
+
+    class Triangle extends Shape {
+        constructor() {
+
+        }
+        describe() {
+
+        }
+    }
+
+    class Rectangle extends Shape {
+        constructor() {
+
+        }
+        describe() {
+
+        }
+    }
+
+    class Square extends Shape {
+        constructor() {
+
+        }
+        describe() {
+
+        }
+    }
 
 
 
