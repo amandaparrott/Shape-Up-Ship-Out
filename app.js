e8df7fa4cef618dfb60d8735d28caa3f6dc83fc8
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-10-09 17:17:46 -0500

    click empty not working

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ window.addEventListener('DOMContentLoaded', function () {
     const rectangleButton = document.getElementById('rectangleButton');
     const rectangleInputH = document.getElementById('rectangleInputH');
     const rectangleInputW = document.getElementById('rectangleInputW');
-    // squareButton = document.getElementById('squarebutton'),
-    // squareInput = document.getElementById('squareInput'),
-    // circleButton = document.getElementById('circleButton'),
-    // circleInput = document.getElementById('circleInput'),
-    // triangleButton = document.getElementById('triangleButton'),
-    // triangleInput = document.getElementById('triangleInput'),
-    // shapeContainer = document.getElementById('shapeContainer'),
-    // shapeInfo = document.getElementById('shapeInfo'),
-    // widthInfo = document.getElementById('widthInfo'),
-    // heightInfo = document.getElementById('heightInfo'),
-    // radiusInfo = document.getElementById('radiusInfo'),
-    // areaInfo = document.getElementById('areaInfo'),
-    // perimeterInfo = document.getElementById('perimeterInfo');
+    const squareButton = document.getElementById('squareButton');
+    const squareInput = document.getElementById('squareInput');
+    const circleButton = document.getElementById('circleButton');
+    const circleInput = document.getElementById('circleInput');
+    const triangleButton = document.getElementById('triangleButton');
+    const triangleInput = document.getElementById('triangleInput');
+    const shapeContainer = document.getElementById('shapeContainer');
+    const shapeInfo = document.getElementById('shapeInfo');
+    const widthInfo = document.getElementById('widthInfo');
+    const heightInfo = document.getElementById('heightInfo');
+    const radiusInfo = document.getElementById('radiusInfo');
+    const areaInfo = document.getElementById('areaInfo');
+    const perimeterInfo = document.getElementById('perimeterInfo');
+
 
 
     class Shape {
@@ -35,77 +36,106 @@ window.addEventListener('DOMContentLoaded', function () {
                 this.div.style.height = `${this.height}px`;
                 this.div.className = "shape";
                 shapeContainer.appendChild(this.div);
+                let x = getRandomNumber();
+                let y = getRandomNumber();
+                this.div.style.left = `${x}px`;
+                this.div.style.top = `${y}px`;
             };
-
-
+            this.div.addEventListener('dblclick', () => this.deleteShape());
         }
-        describe() {
-
+        deleteShape() {
+            this.div.remove();
         }
     }
 
     class Circle extends Shape {
         constructor(height, width) {
             super(height, width);
-            this.div.id = "circle";
+            this.div.classList.add('circle');
+            this.div.addEventListener('click', () => this.describe());
         }
         describe() {
-
-        }
+            shapeInfo.innerText = 'Circle';
+            widthInfo.innerText = `${this.width}`;
+            heightInfo.innerText = `${this.height}`;
+            radiusInfo.innerText = circleInput.value;
+            areaInfo.innerText = Math.floor(Math.PI * circleInput.value * circleInput.value);
+            perimeterInfo.innerText = Math.floor(2 * Math.PI * circleInput.value);
+}
     }
 
     class Triangle extends Shape {
-        constructor() {
+        constructor(height, width) {
             super(height, width);
-            this.div.id = "triangle"
+            this.div.classList.add('triangle');
+            this.div.style.borderBottom = `${this.width}px solid rgb(241, 241, 107)`;
+            this.div.style.borderRight = `${this.height}px solid transparent`;
+            this.div.addEventListener('click', () => this.describe());
         }
         describe() {
-
+            shapeInfo.innerText = 'Triangle';
+            widthInfo.innerText = `${this.width}`;
+            heightInfo.innerText = `${this.height}`;
+            radiusInfo.innerText = `N/A`;
+            areaInfo.innerText = .5 * triangleInput.value * triangleInput.value;
+            perimeterInfo.innerText = Math.floor(2 * triangleInput.value + Math.sqrt(2) * triangleInput.value);
         }
     }
 
     class Rectangle extends Shape {
         constructor(height, width) {
             super(height, width);
-            this.div.id = "rectangle";
-            if (this.div.id === undefined) {
-                
-            }
-
-
+            this.div.classList.add('rectangle');
+            this.div.addEventListener('click', () => this.describe());
         }
         describe() {
-
+            shapeInfo.innerText = 'Rectangle';
+            widthInfo.innerText = `${this.width}`;
+            heightInfo.innerText = `${this.height}`;
+            radiusInfo.innerText = `N/A`;
+            areaInfo.innerText = rectangleInputH.value * rectangleInputW.value;
+            perimeterInfo.innerText = (rectangleInputH.value * 2) + (rectangleInputW.value * 2);
         }
     }
 
     class Square extends Shape {
         constructor(height, width) {
             super(height, width);
-            this.div.id = "square";
+            this.div.classList.add('square');
         }
         describe() {
-
+            shapeInfo.innerText = 'Square';
+            widthInfo.innerText = `${this.width}`;
+            heightInfo.innerText = `${this.height}`;
+            radiusInfo.innerText = `N/A`;
+            areaInfo.innerText = squareInput.value * squareInput.value;
+            perimeterInfo.innerText = (SquareInput.value * 2) + (squareInput.value * 2);
         }
     }
 
     rectangleButton.addEventListener('click', function newRectangle() {
-        new Rectangle(rectangleInputH.value, rectangleInputW.value)
-        if 
+        new Rectangle(rectangleInputH.value, rectangleInputW.value);
     });
 
     squareButton.addEventListener('click', function newSquare() {
         new Square(squareInput.value, squareInput.value);
     });
     circleButton.addEventListener('click', function newCircle() {
-        new Circle(circleInput.value, squareInput.value)
+        new Circle((circleInput.value * 2), (circleInput.value * 2));
     });
 
-    // triangleButton.addEventListener('click', new Triangle());
+    triangleButton.addEventListener('click', function newTriangle() {
+        new Triangle(triangleInput.value, triangleInput.value);
+    });
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 600);
+    };
+
+        
 
 
-
-    // document.getElementById('field').value;
+    
 
     ///end of body
 });
